0e177eefceef9e0894bc4aaf56453ac75a8efede
RiqSodre <riqsodre@gmail.com>
2017-09-26 14:17:24 -0300

    260917 - Adição do menu fixo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
+//MENU ANIMATE//
 $("ul.navbar-nav > li > a").on('click', function(event){
 	event.preventDefault();
-	$("html, body").animate({scrollTop:$(this.hash).offset().top}, 1200);
+
+	$('ul.navbar-nav li').removeClass('active');
+	$(this).parent().addClass('active');
+	$("html, body").animate({scrollTop:$(this.hash).offset().top-51}, 1500);
+})
+
+//MENU FOLLOW//
+$(window).scroll(function(){
+	var scrollTopAtual = $(this).scrollTop();
+
+	if (scrollTopAtual > 150){
+		$('#navegacao').addClass('fixed');
+	}
+	else{
+		$('#navegacao').removeClass('fixed');
+	}
 })
