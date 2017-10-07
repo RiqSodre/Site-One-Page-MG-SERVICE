3c4f4235784e1efb420c6d7f76b921da14633265
ZickkyG <Giovanni.Hessel@Fatec.sp.gov.br>
2017-10-07 18:33:10 -0300

    att071017 - Pequenos ajuster no email/php

--- php/email.php
+++ php/email.php
@@ -14,122 +14,12 @@
 	$assunto = $_POST['input__assunto'];
 	$mensagem = $_POST['input__mensagem'];
 	//Corpo E-mail
-	$arquivo = "<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional //EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
-<html xmlns='http://www.w3.org/1999/xhtml' xmlns='http://www.w3.org/1999/xhtml' xmlns:v='urn:schemas-microsoft-com:vml' xmlns:o='urn:schemas-microsoft-com:office:office'>
-<head>
-
-<!--[if gte mso 9]><xml>
- <o:OfficeDocumentSettings>
-  <o:AllowPNG/>
-  <o:PixelsPerInch>96</o:PixelsPerInch>
- </o:OfficeDocumentSettings>
-</xml><![endif]-->
-
-<style type='text/css' >
-	html, body{ margin:0; padding:0;  }
-	table{ margin-top: 0px !important; padding-top: 0px !important; }
-</style>
-
-<style type='text/css' >
-	[office365] button{ margin:0 !important; padding:0 !important; display:block !important; }
-	[office365] div{ margin:0 !important; padding:0 !important; display:block !important; }
-</style>
-
-
-<style type='text/css'>
-		a img{ color:#000001 !important; }
-
-.wysiwyg-text-align-right{ text-align: right; }
-.wysiwyg-text-align-center { text-align: center; }
-.wysiwyg-text-align-left{ text-align: left; }
-.wysiwyg-text-align-justify{ text-align: justify; }
-
-h1, #email-546794 h1{ text-shadow:none; padding-top:0; padding-right:0; padding-bottom:0; padding-left:0; margin-top:0; margin-right:0; margin-bottom:0; margin-left:0; color:#000000!important; font-weight:400; font-style:normal; font-family:Arial; font-size:36px; line-height:44px; }
-
-h2, #email-546794 h2{ text-shadow:none; padding-top:0; padding-right:0; padding-bottom:0; padding-left:0; margin-top:0; margin-right:0; margin-bottom:0; margin-left:0; color:#000000!important; font-weight:400; font-style:normal; font-family:Arial; font-size:24px; line-height:32px; }
-
-h3, #email-546794 h3{ text-shadow:none; padding-top:0; padding-right:0; padding-bottom:0; padding-left:0; margin-top:0; margin-right:0; margin-bottom:0; margin-left:0; color:#000000!important; font-weight:400; font-style:normal; font-family:Arial; font-size:15px; line-height:21px; }
-
-p, #email-546794 p{ text-shadow:none; padding-top:0; padding-right:0; padding-bottom:0; padding-left:0; margin-top:0; margin-right:0; margin-bottom:0; margin-left:0; color:#000000!important; font-weight:400; font-style:normal; font-family:Calibri; font-size:16px; line-height:24px; }
-
-a, #email-546794 a{ text-shadow:none; padding-top:0; padding-right:0; padding-bottom:0; padding-left:0; margin-top:0; margin-right:0; margin-bottom:0; margin-left:0; color:#1122CC!important; text-decoration:underline; }
-
-
-.h1_color_span_wrapper{ color: #000000; }
-.h2_color_span_wrapper{ color: #000000; }
-.h3_color_span_wrapper{ color: #000000; }
-.p_color_span_wrapper{ color: #000000; }
-.a_color_span_wrapper{ color: #1122CC; }
-
-
-		.mi-all{ display: block; }
-		.mi-desktop{ display: block; }
-
-	.mi-mobile{
-		display: none;
-		font-size: 0; 
-		max-height: 0; 
-		line-height: 0; 
-		padding: 0;
-		float: left;
-		overflow: hidden;
-		mso-hide: all; /* hide elements in Outlook 2007-2013 */
-	}
-
-
-</style>
-
-<style type='text/css' >
-
-	div, p, a, li, td { -webkit-text-size-adjust:none; }
-	
-	@media only screen and (max-device-width: 480px), screen and (max-width: 480px), screen and (orientation: landscape) and (max-width: 630px) {
-		
-		/* very important! all except 'all' and this current type get a display:none; */
-		.mi-desktop{ display: none !important; }
-
-		/* then show the mobile one */
-		.mi-mobile{ 
-			display: block !important;
-			font-size: 12px !important;
-			max-height: none !important;
-			line-height: 1.5 !important;
-			float: none !important;
-			overflow: visible !important;
-		}
-	}
-
-</style>
-
-	
-   <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
-   
-</head><body id='email-546794' style='background-color: #FFFFFF; font-family: sans-serif; margin-top: 0px !important; padding-top: 0px !important' bgcolor='#FFFFFF'><table width='1' height='1' cellpadding='0' cellspacing='0' border='0'><tr><td style='font-size: 0px; line-height: 0px'><img src='http://t.inkbrush.com/p/cp/543cbae70a9d728d/o.gif' width='1' height='1' /></td></tr></table><table align='center' cellpadding='0' cellspacing='0' width='100%' style='background-color: #FFFFFF; border: 0; border-collapse: collapse; border-spacing: 0; margin: 0px 0 0; padding: 0' bgcolor='#FFFFFF'><tr align='center' style='border: 0; border-collapse: collapse; border-spacing: 0'>
-						<td align='center' valign='top' style='border: 0; border-collapse: collapse; border-spacing: 0'>
-									
-<div class='mi-all' style='display: block'><table width='665' class='mi-all' align='center' cellspacing='0' cellpadding='0' border='0' style='border: 0; border-collapse: collapse; border-spacing: 0; display: block; margin: 0; min-width: 665px; padding: 0'><tbody><tr align='left' style='border: 0; border-collapse: collapse; border-spacing: 0'><td><table cellspacing='0' cellpadding='0' border='0' style='border: 0; border-collapse: collapse; border-spacing: 0; min-width: 665px'><tbody><tr style='border: 0; border-collapse: collapse; border-spacing: 0'><td align='left' valign='top' style='height: 454px; width: 14px'><img border='0' src='http://grupomgservice.com.br/images/image-224ff9e6660bdb2fb1c411617d5c5cdc6fb8272e.jpg' style='border: 0; display: block; height: 454px; line-height: 0; max-height: 454px; max-width: 14px; min-height: 454px; min-width: 14px; width: 14px' /></td><td align='left' valign='top'><table align='center' cellspacing='0' cellpadding='0' border='0' style='border: 0; border-collapse: collapse; border-spacing: 0; margin: 0; min-width: 637px; padding: 0'><tbody><tr align='left' style='border: 0; border-collapse: collapse; border-spacing: 0'><td><table cellspacing='0' cellpadding='0' border='0' style='border: 0; border-collapse: collapse; border-spacing: 0; min-width: 637px'><tbody><tr style='border: 0; border-collapse: collapse; border-spacing: 0'><td align='left' valign='top' style='height: 65px; width: 637px'><img border='0' src='http://grupomgservice.com.br/images/image-5e085a0cfd5e85394280bfbda45379833f7a428e.jpg' style='border: 0; display: block; height: 65px; line-height: 0; max-height: 65px; max-width: 637px; min-height: 65px; min-width: 637px; width: 637px' /></td></tr></tbody></table></td></tr><tr align='left' style='border: 0; border-collapse: collapse; border-spacing: 0'><td><table cellspacing='0' cellpadding='0' border='0' style='border: 0; border-collapse: collapse; border-spacing: 0; min-width: 637px'><tbody><tr style='border: 0; border-collapse: collapse; border-spacing: 0'><td align='left' valign='top' style='height: 25px; width: 43px'><img border='0' src='http://grupomgservice.com.br/images/image-9d4b6d963ef111c29a3c5d814d6ae3a3a23cf44a.jpg' style='border: 0; display: block; height: 25px; line-height: 0; max-height: 25px; max-width: 43px; min-height: 25px; min-width: 43px; width: 43px' /></td><td width='150' align='left' valign='middle' height='25' style='background-color: #EEEEEE; height: 25px; width: 150px' bgcolor='#EEEEEE'><p style='color: #000000 !important; font-family: Calibri; font-size: 16px; font-style: normal; font-weight: 400; line-height: 24px;  margin-top:0; padding-top:0; margin-right:0; padding-right:0; margin-bottom:0; padding-bottom:0; margin-left:0; padding-left:0;  text-align: left; text-shadow: none' align='left'><span class='p_color_span_wrapper' style='color: #000000'>$assunto</span></p></td><td align='left' valign='top' style='height: 25px; width: 444px'><img border='0' src='http://grupomgservice.com.br/images/image-924b57a3e6f6926c2dc75f95cba83cbf8344b41a.jpg' style='border: 0; display: block; height: 25px; line-height: 0; max-height: 25px; max-width: 444px; min-height: 25px; min-width: 444px; width: 444px' /></td></tr></tbody></table></td></tr><tr align='left' style='border: 0; border-collapse: collapse; border-spacing: 0'><td><table cellspacing='0' cellpadding='0' border='0' style='border: 0; border-collapse: collapse; border-spacing: 0; min-width: 637px'><tbody><tr style='border: 0; border-collapse: collapse; border-spacing: 0'><td align='left' valign='top' style='height: 62px; width: 637px'><img border='0' src='http://grupomgservice.com.br/images/image-df426bfe68b17a64abf226c71901f36de5f564e6.jpg' style='border: 0; display: block; height: 62px; line-height: 0; max-height: 62px; max-width: 637px; min-height: 62px; min-width: 637px; width: 637px' /></td></tr></tbody></table></td></tr><tr align='left' style='border: 0; border-collapse: collapse; border-spacing: 0'><td><table cellspacing='0' cellpadding='0' border='0' style='border: 0; border-collapse: collapse; border-spacing: 0; min-width: 637px'><tbody><tr style='border: 0; border-collapse: collapse; border-spacing: 0'><td align='left' valign='top' style='height: 25px; width: 44px'><img border='0' src='http://grupomgservice.com.br/images/image-de9315842423c9799ce367b613936ecaa991c7c5.jpg' style='border: 0; display: block; height: 25px; line-height: 0; max-height: 25px; max-width: 44px; min-height: 25px; min-width: 44px; width: 44px' /></td><td width='273' align='left' valign='middle' height='25' style='background-color: #EEEEEE; height: 25px; width: 273px' bgcolor='#EEEEEE'><p style='color: #000000 !important; font-family: Calibri; font-size: 16px; font-style: normal; font-weight: 400; line-height: 24px;  margin-top:0; padding-top:0; margin-right:0; padding-right:0; margin-bottom:0; padding-bottom:0; margin-left:0; padding-left:0;  text-align: left; text-shadow: none' align='left'><span class='p_color_span_wrapper' style='color: #000000'>$nome</span></p></td><td align='left' valign='top' style='height: 25px; width: 50px'><img border='0' src='http://grupomgservice.com.br/images/image-57bd693b1ff07d8daaa0efc5a38f58859a4145e0.jpg' style='border: 0; display: block; height: 25px; line-height: 0; max-height: 25px; max-width: 50px; min-height: 25px; min-width: 50px; width: 50px' /></td><td width='255' align='left' valign='middle' height='25' style='background-color: #EEEEEE; height: 25px; width: 255px' bgcolor='#EEEEEE'><p style='color: #000000 !important; font-family: Calibri; font-size: 16px; font-style: normal; font-weight: 400; line-height: 24px;  margin-top:0; padding-top:0; margin-right:0; padding-right:0; margin-bottom:0; padding-bottom:0; margin-left:0; padding-left:0;  text-shadow: none'><span class='p_color_span_wrapper' style='color: #000000'>$email</span></p></td><td align='left' valign='top' style='height: 25px; width: 15px'><img border='0' src='http://grupomgservice.com.br/images/image-3a0897b90756cd5a290956d93cfe12babf472730.jpg' style='border: 0; display: block; height: 25px; line-height: 0; max-height: 25px; max-width: 15px; min-height: 25px; min-width: 15px; width: 15px' /></td></tr></tbody></table></td></tr><tr align='left' style='border: 0; border-collapse: collapse; border-spacing: 0'><td><table cellspacing='0' cellpadding='0' border='0' style='border: 0; border-collapse: collapse; border-spacing: 0; min-width: 637px'><tbody><tr style='border: 0; border-collapse: collapse; border-spacing: 0'><td align='left' valign='top' style='height: 60px; width: 637px'><img border='0' src='http://grupomgservice.com.br/images/image-a47579746b032ef1d061597f4973de4fc32d6d2c.jpg' style='border: 0; display: block; height: 60px; line-height: 0; max-height: 60px; max-width: 637px; min-height: 60px; min-width: 637px; width: 637px' /></td></tr></tbody></table></td></tr><tr align='left' style='border: 0; border-collapse: collapse; border-spacing: 0'><td><table cellspacing='0' cellpadding='0' border='0' style='border: 0; border-collapse: collapse; border-spacing: 0; min-width: 637px'><tbody><tr style='border: 0; border-collapse: collapse; border-spacing: 0'><td align='left' valign='top' style='height: 25px; width: 43px'><img border='0' src='http://grupomgservice.com.br/images/image-0d111d57b92f28dbe2ba382598140878991c46a1.jpg' style='border: 0; display: block; height: 25px; line-height: 0; max-height: 25px; max-width: 43px; min-height: 25px; min-width: 43px; width: 43px' /></td><td width='273' align='left' valign='middle' height='25' style='background-color: #EEEEEE; height: 25px; width: 273px' bgcolor='#EEEEEE'><p style='color: #000000 !important; font-family: Calibri; font-size: 16px; font-style: normal; font-weight: 400; line-height: 24px;  margin-top:0; padding-top:0; margin-right:0; padding-right:0; margin-bottom:0; padding-bottom:0; margin-left:0; padding-left:0;  text-align: left; text-shadow: none' align='left'><span class='p_color_span_wrapper' style='color: #000000'>$telefone</span></p></td><td align='left' valign='top' style='height: 25px; width: 50px'><img border='0' src='http://grupomgservice.com.br/images/image-79401168f386f09d54f0a25e49cc3d54373b7b30.jpg' style='border: 0; display: block; height: 25px; line-height: 0; max-height: 25px; max-width: 50px; min-height: 25px; min-width: 50px; width: 50px' /></td><td width='255' align='left' valign='middle' height='25' style='background-color: #EEEEEE; height: 25px; width: 255px' bgcolor='#EEEEEE'><p style='color: #000000 !important; font-family: Calibri; font-size: 16px; font-style: normal; font-weight: 400; line-height: 24px;  margin-top:0; padding-top:0; margin-right:0; padding-right:0; margin-bottom:0; padding-bottom:0; margin-left:0; padding-left:0;  text-shadow: none'><span class='p_color_span_wrapper' style='color: #000000'>$telefonetipo</span></p></td><td align='left' valign='top' style='height: 25px; width: 16px'><img border='0' src='http://grupomgservice.com.br/images/image-cafc90558c8bd4b6ab4a5b5051d2e44e8ff070b5.jpg' style='border: 0; display: block; height: 25px; line-height: 0; max-height: 25px; max-width: 16px; min-height: 25px; min-width: 16px; width: 16px' /></td></tr></tbody></table></td></tr><tr align='left' style='border: 0; border-collapse: collapse; border-spacing: 0'><td><table cellspacing='0' cellpadding='0' border='0' style='border: 0; border-collapse: collapse; border-spacing: 0; min-width: 637px'><tbody><tr style='border: 0; border-collapse: collapse; border-spacing: 0'><td align='left' valign='top' style='height: 63px; width: 637px'><img border='0' src='http://grupomgservice.com.br/images/image-6494cfbced1061c8501ddf4cc2f2687c8cde5ba5.jpg' style='border: 0; display: block; height: 63px; line-height: 0; max-height: 63px; max-width: 637px; min-height: 63px; min-width: 637px; width: 637px' /></td></tr></tbody></table></td></tr><tr align='left' style='border: 0; border-collapse: collapse; border-spacing: 0'><td><table cellspacing='0' cellpadding='0' border='0' style='border: 0; border-collapse: collapse; border-spacing: 0; min-width: 637px'><tbody><tr style='border: 0; border-collapse: collapse; border-spacing: 0'><td align='left' valign='top' style='height: 77px; width: 43px'><img border='0' src='http://grupomgservice.com.br/images/image-bf2d8c381cc3d388a4c0128518a286f19398184a.jpg' style='border: 0; display: block; height: 77px; line-height: 0; max-height: 77px; max-width: 43px; min-height: 77px; min-width: 43px; width: 43px' /></td><td width='578' align='left' valign='top' height='77' style='background-color: #EEEEEE; height: 77px; width: 578px' bgcolor='#EEEEEE'><div style='height: 76px; max-height: 76px; max-width: 577px; overflow: hidden; width: 577px'><p style='color: #000000 !important; font-family: Calibri; font-size: 16px; font-style: normal; font-weight: 400; line-height: 24px;  margin-top:0; padding-top:0; margin-right:0; padding-right:0; margin-bottom:0; padding-bottom:0; margin-left:0; padding-left:0;  text-shadow: none'><textarea class='p_color_span_wrapper' style='color: #000000;resize:none;width: 550px; height: 70px;border:0;background-color: #EEEEEE;font-style:normal; font-family:Calibri; font-size:16px; line-height:24px;' maxlength='400'>$mensagem</textarea></p></div></td><td align='left' valign='top' style='height: 77px; width: 16px'><img border='0' src='http://grupomgservice.com.br/images/image-d322a0da81b0be5e0ffdd9f73a1e807d9a0495a0.jpg' style='border: 0; display: block; height: 77px; line-height: 0; max-height: 77px; max-width: 16px; min-height: 77px; min-width: 16px; width: 16px' /></td></tr></tbody></table></td></tr><tr align='left' style='border: 0; border-collapse: collapse; border-spacing: 0'><td><table cellspacing='0' cellpadding='0' border='0' style='border: 0; border-collapse: collapse; border-spacing: 0; min-width: 637px'><tbody><tr style='border: 0; border-collapse: collapse; border-spacing: 0'><td align='left' valign='top' style='height: 52px; width: 637px'><img border='0' src='http://grupomgservice.com.br/images/image-c1cc0390c6979529eecf56080d6c2d28120697c7.jpg' style='border: 0; display: block; height: 52px; line-height: 0; max-height: 52px; max-width: 637px; min-height: 52px; min-width: 637px; width: 637px' /></td></tr></tbody></table></td></tr></tbody></table></td><td align='left' valign='top' style='height: 454px; width: 14px'><img border='0' src='http://grupomgservice.com.br/images/image-d9398dc01a4906a9825cbf55b9fbdd097ace7d44.jpg' style='border: 0; display: block; height: 454px; line-height: 0; max-height: 454px; max-width: 14px; min-height: 454px; min-width: 14px; width: 14px' /></td></tr></tbody></table></td></tr></tbody></table></div>
-
-
-						</td>
-					</tr>
-			</table>
-
-			
-
-			<div style='display: none; font: 15px courier; white-space: nowrap'>
-			                      
-			                      
-			               
-			</div>
-
-  </body>
- </html>
-";
+	$arquivo = str_replace(array('$assunto', '$nome', '$email', '$telefone', '%telefonetipo', '$mensagem'), array($assunto, $nome, $email, $telefone, $telefonetipo, $mensagem) ,file_get_contents('../mail.php'));
 	//Enviar email
 	$remetente = "noreply@grupomgservice.com.br";
 	$destino = "contato@grupomgservice.com.br";
 	$headers  = 'MIME-Version: 1.0'."\r\n";
-	$headers .= 'Content-type: text/html; charset=ISO-8859-1'."\r\n";
+	$headers .= 'Content-type: text/html; charset=UTF-8'."\r\n";
 	$headers .= 'From: Contato Site <noreply@grupomgservice.com.br>';
 	$enviaremail = mail($destino, $assunto, $arquivo, $headers, "-f$remetente");
 	if($enviaremail){
