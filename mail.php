20ddf86dde7bcdfebf4e2d094ebc9ffdba99a369
ZickkyG <Giovanni.Hessel@Fatec.sp.gov.br>
2017-10-07 15:36:07 -0300

    att071017 - Implementado Layout de email funcional

--- mail.php
+++ mail.php
@@ -1,20 +1,4 @@
-<?php
-	//Entrada dos dados
-	$nome = $_POST['input__nome'];
-	if(isset($POST['input__email'])){
-		$email = $_POST['input__email'];
-	}
-	if (isset($_POST["input__email"]) && !empty($_POST["input__email"])) {
-	    $email = $_POST['input__email']; 
-	}else{  
-	    $email = "Não informado.";
-	}
-	$telefone = $_POST['input__telefone'];
-	$telefonetipo = $_POST['input__telefonetipo'];
-	$assunto = $_POST['input__assunto'];
-	$mensagem = $_POST['input__mensagem'];
-	//Corpo E-mail
-	$arquivo = "<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional //EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional //EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
 <html xmlns='http://www.w3.org/1999/xhtml' xmlns='http://www.w3.org/1999/xhtml' xmlns:v='urn:schemas-microsoft-com:vml' xmlns:o='urn:schemas-microsoft-com:office:office'>
 <head>
 
@@ -123,19 +107,4 @@ a, #email-546794 a{ text-shadow:none; padding-top:0; padding-right:0; padding-bo
 			</div>
 
   </body>
- </html>
-";
-	//Enviar email
-	$remetente = "noreply@grupomgservice.com.br";
-	$destino = "contato@grupomgservice.com.br";
-	$headers  = 'MIME-Version: 1.0'."\r\n";
-	$headers .= 'Content-type: text/html; charset=ISO-8859-1'."\r\n";
-	$headers .= 'From: Contato Site <noreply@grupomgservice.com.br>';
-	$enviaremail = mail($destino, $assunto, $arquivo, $headers, "-f$remetente");
-	if($enviaremail){
-		$retorno = "Mensagem enviada com sucesso, aguarde nosso contato.";
-	} else {
-		$retorno = "Ops, houve um erro. Tente novamente mais tarde.";
-	}
-	echo $retorno;
-?>
+</html>
